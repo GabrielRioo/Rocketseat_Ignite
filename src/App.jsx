ba6a17a9da523ab084f559957ea43cee058ce75f
Gabriel Paiva Rio <gabrielpaivario@gmail.com>
2023-05-27 17:28:00 -0300

    Adding the header and the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
+import {Post} from './Post'
+import './assets/global.css'
+import { Header } from './components/Header'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+     <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <Post 
+          author="Gabriel Rio" 
+          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid nam soluta fuga dolorum atque quis est impedit quos labore eaque minima ipsa eveniet reiciendis, voluptatibus incidunt quibusdam iure blanditiis sapiente."
+        />
+        <Post 
+          author="Diego Fernandes" 
+          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid nam soluta fuga dolorum atque quis est impedit quos labore eaque minima ipsa eveniet reiciendis, voluptatibus incidunt quibusdam iure blanditiis sapiente."
+        />
+      </main>
+     </div>
     </>
   )
 }
 
-export default App
