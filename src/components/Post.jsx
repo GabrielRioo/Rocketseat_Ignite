1a8aca7d230b2f94ed8ff36e98da08a4601770db
GabrielRioo <gabrielpaivario@gmail.com>
2023-05-28 20:08:49 -0300

    Creating the textbox area to write a comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(props) {
@@ -28,6 +29,21 @@ export function Post(props) {
                     <a href=''>#nlw </a> {' '}
                     <a href=''>#rocketseat</a>
                 </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder='Deixe um Comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
             </div>
         </article>
     )
